f92fdbf93166e7d1f318f8b74f15fc1287a5698f
李翔宇 <xybevis@gmail.com>
2022-09-19 18:49:16 +0800

    1

--- src/test/java/com/ql/util/express/test/InTest.java
+++ src/test/java/com/ql/util/express/test/InTest.java
@@ -13,11 +13,13 @@ public class InTest {
         String express1 = "2 in (2, 3) ";
         String express2 = "2 in a";
         String express3 = "2 in b";
+        String express4 = "from_system in (2,3)";
 
-        ExpressRunner runner = new ExpressRunner(true, true);
+        ExpressRunner runner = new ExpressRunner(true, false);
         DefaultContext<String, Object> context = new DefaultContext<>();
         int[] a = {1, 2, 3};
         context.put("a", a);
+        context.put("from_system", 2);
         List<Integer> b = new ArrayList<>();
         b.add(2);
         b.add(3);
@@ -26,5 +28,6 @@ public class InTest {
         System.out.println(runner.execute(express1, context, null, false, false));
         System.out.println(runner.execute(express2, context, null, false, false));
         System.out.println(runner.execute(express3, context, null, false, false));
+        System.out.println(runner.execute(express4, context, null, false, false));
     }
 }
